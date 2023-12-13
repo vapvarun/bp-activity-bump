202566160d7c5f81f72946a9880f3dcefc70958c
Abhishek <pallavi@wbcomdesigns.com>
2023-12-13 17:18:47 +0530

    Updated top banner and Links

--- admin/wbcom/templates/wbcom-license-page.php
+++ admin/wbcom/templates/wbcom-license-page.php
@@ -1,7 +1,7 @@
 <div class="wrap">
 	<div class="wbcom-bb-plugins-offer-wrapper">
 		<div id="wb_admin_logo">
-			<a href="https://wbcomdesigns.com/downloads/buddypress-community-bundle/" target="_blank">
+			<a href="https://wbcomdesigns.com/downloads/buddypress-community-bundle/?utm_source=pluginoffernotice&utm_medium=community_banner" target="_blank">
 				<img src="<?php echo esc_url( BP_ACTIVITY_BUMP_PLUGIN_URL ) . 'admin/wbcom/assets/imgs/wbcom-offer-notice.png'; ?>">
 			</a>
 		</div>
@@ -10,9 +10,12 @@
 		<div class="wbcom_admin_header-wrapper">
 			<div id="wb_admin_plugin_name">
 				<?php esc_html_e( 'BuddyPress Activity Bump', 'bp-activity-bump' ); ?>
-				<span><?php /* translators: %s: */
+				<span>
+				<?php
+				/* translators: %s: */
 				printf( esc_html__( 'Version %s', 'bp-activity-bump' ), esc_attr( BP_ACTIVITY_BUMP_VERSION ) );
-				?></span>
+				?>
+				</span>
 			</div>
 			<?php echo do_shortcode( '[wbcom_admin_setting_header]' ); ?>
 		</div>
