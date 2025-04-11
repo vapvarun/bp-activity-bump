440cac3de25157da0a74f37fd1480e36c45ce259
Nitin Patil <pallavi@wbcomdesigns.com>
2025-04-11 11:31:32 +0530

    Remove unwanted files and code of wbcom folder
    https://3.basecamp.com/5798509/buckets/37595412/card_tables/cards/8534566553

--- admin/bp-bump-admin.php
+++ admin/bp-bump-admin.php
@@ -142,9 +142,6 @@ if ( ! class_exists( 'BP_ACTIVITY_BUMP_ADMIN_SETIING' ) ) {
 			<div class="wrap">
 				<div class="wbcom-bb-plugins-offer-wrapper">
 					<div id="wb_admin_logo">
-						<a href="https://wbcomdesigns.com/downloads/buddypress-community-bundle/?utm_source=pluginoffernotice&utm_medium=community_banner" target="_blank">
-							<img src="<?php echo esc_url( BP_ACTIVITY_BUMP_PLUGIN_URL ) . 'admin/wbcom/assets/imgs/wbcom-offer-notice.png'; ?>">
-						</a>
 					</div>
 				</div>
 				<div class="wbcom-wrap">
